2ca6249578eeefda314b3f1fe79ba4350df42883
Gabriel Carvalho <scarvalho.gabriel@gmail.com>
2023-05-09 21:58:41 -0300

    added to top button

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const nav = document.getElementById("navigation");
+const toTopButton = document.getElementById("button-to-top");
 
 window.addEventListener("scroll", onScroll);
 
 function onScroll() {
   changeNavOnScroll();
+  showToTopButton();
 }
 
 function changeNavOnScroll() {
@@ -11,6 +13,14 @@ function changeNavOnScroll() {
     nav.classList.add("scroll");
   } else {
     nav.classList.remove("scroll");
+  }
+}
+
+function showToTopButton() {
+  if (scrollY > 650) {
+    toTopButton.classList.add("show");
+  } else {
+    toTopButton.classList.remove("show");
   }
 }
 
